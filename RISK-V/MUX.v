a8f16c4c4f370d800e365d75a483269e707d7b9e
Surbhi <122719916+Surbhi2023@users.noreply.github.com>
2025-08-08 22:01:37 +0530

    Create MUX.v

---
+++ RISK-V/MUX.v
@@ -0,0 +1,19 @@
+// Multiplexers 
+module Mux1(sel1, A1, B1, Mux1_out);
+  input sel1;
+  input [31:0] A1, B1;
+  output [31:0] Mux1_out;
+  assign Mux1_out = (sel1==1'b0) ? A1 : B1;
+endmodule 
+module Mux2(sel1, A1, B1, Mux1_out);
+  input sel1;
+  input [31:0] A1, B1;
+  output [31:0] Mux1_out;
+  assign Mux1_out = (sel1==1'b0) ? A2 : B2;
+endmodule 
+module Mux1(sel1, A1, B1, Mux1_out);
+  input sel1;
+  input [31:0] A1, B1;
+  output [31:0] Mux1_out;
+  assign Mux1_out = (sel1==1'b0) ? A3 : B3;
+endmodule 
